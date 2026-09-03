e30b87a31ae9cb2044fc63523340f8ba31d7a58c
Karunakar Mishra <karunakarmishra2006@gmail.com>
2026-09-03 14:40:43 +0530

    mini project on cards update

--- 04-cards-project/src/components/Card.jsx
+++ 04-cards-project/src/components/Card.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Bookmark } from "lucide-react";
-const Card = () => {
+const Card = (props) => {
   return (
     <div className="card">
       <div>
@@ -15,19 +15,19 @@ const Card = () => {
         </div>
         <div className="center">
           <h3>
-            Amazon <span>5 days ago</span>
+            {props.companyName} <span>{props.datePosted}</span>
           </h3>
-          <h2>Senior UI/UX Engineer</h2>
+          <h2>{props.post}</h2>
           <div className="tag">
-            <h4>Part Time</h4>
-            <h4>Senior Level</h4>
+            <h4>{props.tag1}</h4>
+            <h4>{props.tag2}</h4>
           </div>
         </div>
       </div>
       <div className="bottom">
         <div>
-          <h3>$120/hr</h3>
-          <p>Mumbai, India</p>
+          <h3>{props.pay}</h3>
+          <p>{props.location}</p>
         </div>
         <button>Apply Now</button>
       </div>
